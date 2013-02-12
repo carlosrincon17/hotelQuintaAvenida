e3539b5055094b9705500b037c6c42267868c3f4
Carlos <Carlos@CarlosRincon>
2013-02-12 15:38:55 -0500

    arreglo metodos listar reservas

--- src/java/dao/ReservaHabitacion_DAO.java
+++ src/java/dao/ReservaHabitacion_DAO.java
@@ -90,7 +90,7 @@ public class ReservaHabitacion_DAO {
 
         ArrayList<ReservaHabitacion_DTO> reserva = new ArrayList<>();
         //String sql="select * from reserva where id_cliente='"+cliente.getDocumento()+"'";
-        String sql = "select * from reserva inner join reserva_habitacion on reserva.id_reserva=reserva_habitacion.id_reserva_habitacion where reserva.id_cliente = ? AND estado='Espera'";
+        String sql = "select * from reserva inner join reserva_habitacion on reserva.id_reserva=reserva_habitacion.id_reserva_habitacion where reserva.id_cliente = ? AND reserva.activa='1'";
         Object[] p = new Object[1];
         p[0] = cliente.getDocumento();
         ResultSet rs = BaseDeDatos.getInstance().ejecutarSQL(sql, p);
